2c02e1197b9d44c8565c26a8b2225e0abd287167
Vincent Fontesse <vincent.fontesse@ac-dijon.fr>
2022-12-08 16:51:26 +0100

    build(libs): update to react 18.2.0

--- app/client/main.jsx
+++ app/client/main.jsx
@@ -1,8 +1,10 @@
-import React from 'react';
-import { Meteor } from 'meteor/meteor';
-import { render } from 'react-dom';
-import { App } from '/imports/ui/App';
+import React from "react";
+import { Meteor } from "meteor/meteor";
+import { App } from "/imports/ui/App";
+import { createRoot } from "react-dom/client";
 
 Meteor.startup(() => {
-  render(<App/>, document.getElementById('react-target'));
+  const container = document.getElementById("react-target");
+  const root = createRoot(container);
+  root.render(<App tab="home" />);
 });
