7e0bb4fa09ac8aaa732e0df75a818b3e07701543
jason-lavigne <jasondevweb@gmail.com>
2022-12-14 14:23:33 +0100

    feat(main): add router

--- app/client/main.jsx
+++ app/client/main.jsx
@@ -1,10 +1,27 @@
-import React from "react";
-import { Meteor } from "meteor/meteor";
-import { App } from "/imports/ui/App";
-import { createRoot } from "react-dom/client";
+import React from 'react';
+import ReactDOM, { createRoot } from 'react-dom/client';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import { Meteor } from 'meteor/meteor';
+import { App } from '/imports/ui/App';
 
 Meteor.startup(() => {
-  const container = document.getElementById("react-target");
+  const container = document.getElementById('react-target');
   const root = createRoot(container);
-  root.render(<App tab="home" />);
+  const router = createBrowserRouter([
+    {
+      path: '/',
+      element: <App tab="home" />,
+    },
+    {
+      path: 'about',
+      element: <div>About</div>,
+    },
+  ]);
+  root.render(
+    <>
+      <React.StrictMode>
+        <RouterProvider router={router} />
+      </React.StrictMode>
+    </>,
+  );
 });
