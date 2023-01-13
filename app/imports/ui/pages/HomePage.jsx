c12f582a7fbb6c33466e43265740ea96e56e7e43
jason-lavigne <jasondevweb@gmail.com>
2023-01-13 14:42:16 +0100

    refactor(pages): manage pages

--- app/imports/ui/pages/HomePage.jsx
+++ app/imports/ui/pages/HomePage.jsx
@@ -1,17 +1,25 @@
-import React, { useContext } from 'react';
+import React, { useContext, useEffect } from 'react';
 import FormProvider from '../contexts/FormContext';
 import UserProvider from '../contexts/UserContext';
 
 import { MainLayout } from '../layouts/MainLayout';
+import Header from '../components/header/Header';
+import { Paper } from '@mui/material';
+import { Link, Navigate, Outlet, useNavigate } from 'react-router-dom';
+import { UserContext } from '../contexts/UserContext';
 
 export const HomePage = () => {
+  const { user } = useContext(UserContext);
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    if (user) navigate('/builder');
+  }, [user]);
+
   return (
-    <UserProvider>
-      <FormProvider>
-        <MainLayout>
-          <div>Bienvenue sur la page d'accueil</div>
-        </MainLayout>
-      </FormProvider>
-    </UserProvider>
+    <>
+      <p>Binvenue dans la nouvelle application</p>
+      <button onClick={() => navigate('/login')}>Se connecter</button>
+    </>
   );
 };
