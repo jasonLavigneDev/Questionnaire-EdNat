1091c0ef36b3be54882c4f0ae24c68c0e2bed4e4
Vincent Fontesse <vincent.fontesse@ac-dijon.fr>
2023-01-18 15:32:16 +0100

    feat(select): make select running

--- app/imports/ui/components/inputs/Select/SelectInput.jsx
+++ app/imports/ui/components/inputs/Select/SelectInput.jsx
@@ -8,19 +8,24 @@ import { AnswerContext } from '../../../contexts/AnswerContext';
 export const SelectInput = ({ title, choices, answerMode, questionId }) => {
   const [answer, setAnswer] = useState('');
   const { addAnswers } = useContext(AnswerContext);
+
+  const handleChange = (event) => {
+    setAnswer(event.target.value);
+    if (answerMode) addAnswers(questionId, event.target.value);
+  };
+
   return (
     <div>
       <FormControl fullWidth>
         <InputLabel id="selectInput-title">{title}</InputLabel>
-        <Select labelId="selectInput-title" value={title} label={title}>
+        <Select labelId="selectInput-title" label={title} value={answer} onChange={handleChange}>
           {choices.map((choice) => (
-            <div key={uuidv4()}>
-              <MenuItem value={choice}>{choice}</MenuItem>
-            </div>
+            <MenuItem key={uuidv4()} value={choice}>
+              {choice}
+            </MenuItem>
           ))}
         </Select>
       </FormControl>
-      {answerMode && <button onClick={() => addAnswers(questionId, answer)}>confirmer cette reponse</button>}
     </div>
   );
 };
