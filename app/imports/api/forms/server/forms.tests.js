265af8af8363ed6c52a868e90f91b7ea58f5c386
Bruno Boiget <bruno.boiget@ac-dijon.fr>
2023-01-20 17:39:55 +0100

    test(forms): add missing test

--- app/imports/api/forms/server/forms.tests.js
+++ app/imports/api/forms/server/forms.tests.js
@@ -4,10 +4,11 @@
 import { assert } from 'chai';
 import { Factory } from 'meteor/dburles:factory';
 import { faker } from '@faker-js/faker';
+import { Accounts } from 'meteor/accounts-base';
 import Forms from '../forms';
 import './factories';
 import { genFormComponent } from './factories';
-import { createForm } from '../methods';
+import { createForm, getUserForms } from '../methods';
 
 describe('forms', function () {
   describe('mutators', function () {
@@ -17,26 +18,54 @@ describe('forms', function () {
     });
   });
   describe('methods', function () {
+    let userId;
     beforeEach(function () {
       Forms.remove({});
+      Meteor.users.remove({});
+      const email = faker.internet.email();
+      userId = Accounts.createUser({
+        email,
+        username: email,
+        password: 'toto',
+      });
     });
-    it('does create a form', function () {
+    it('does create a form', async function () {
       const newform = {
         title: 'yo',
         desc: faker.lorem.sentence(),
         isModel: faker.datatype.boolean(),
         isPublic: faker.datatype.boolean(),
-        owner: faker.name.middleName(),
         components: Array.from({ length: faker.datatype.number({ min: 1, max: 10 }) }, () => genFormComponent()),
       };
-      createForm._execute({}, newform);
-      const form = Forms.findOne({ title: 'yo' });
+      await createForm._execute({ userId }, newform);
+      const form = await Forms.findOneAsync({ title: 'yo' });
       assert.typeOf(form, 'object');
     });
     it('does get a form', async function () {
       const newformId = Factory.create('form')._id;
       const form = await Meteor.callAsync('forms.getOne', newformId);
       assert.typeOf(form, 'object');
+    });
+    it('does get user forms', async function () {
+      const newform = {
+        title: 'yo',
+        desc: faker.lorem.sentence(),
+        isModel: faker.datatype.boolean(),
+        isPublic: faker.datatype.boolean(),
+        components: Array.from({ length: faker.datatype.number({ min: 1, max: 10 }) }, () => genFormComponent()),
+      };
+      await createForm._execute({ userId }, newform);
+      const newform2 = {
+        title: 'yo2',
+        desc: faker.lorem.sentence(),
+        isModel: faker.datatype.boolean(),
+        isPublic: faker.datatype.boolean(),
+        components: Array.from({ length: faker.datatype.number({ min: 1, max: 10 }) }, () => genFormComponent()),
+      };
+      await createForm._execute({ userId: faker.name.middleName() }, newform2);
+      const forms = await getUserForms._execute({ userId });
+      assert.equal(forms.length, 1);
+      assert.equal(forms[0].title, 'yo');
     });
   });
 });
