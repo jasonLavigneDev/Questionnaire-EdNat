514b424d1e93d3a90cee4a2b6ea8112c009481b3
Corentin Faisy <corentin.faisy@ac-dijon.fr>
2023-01-25 10:54:41 +0100

    add auto redirect on already answered form

--- app/imports/ui/pages/FormVisualizer.jsx
+++ app/imports/ui/pages/FormVisualizer.jsx
@@ -1,23 +1,47 @@
 import React, { useContext, useEffect } from 'react';
-import { useLoaderData } from 'react-router-dom';
+import { useState } from 'react';
+import { Navigate, useLoaderData, useNavigate } from 'react-router-dom';
 import { Visualizer } from '../components/form/Visualizer';
+import { MsgError } from '../components/system/MsgError';
 import { FormContext } from '../contexts/FormContext';
+import { UserContext } from '../contexts/UserContext';
 
 export const FormVisualizer = () => {
   const formFromBDD = useLoaderData();
 
   const { form, setForm } = useContext(FormContext);
+  const { user } = useContext(UserContext);
+  const [errorMessage, setErrorMessage] = useState('');
+
+  const navigate = useNavigate();
 
   useEffect(() => {
     setForm(formFromBDD);
   }, []);
 
+  const hasAlreadyRespond = () => {
+    if (!form.formAnswers || form.formAnswers.length === 0) {
+      return false;
+    } else {
+      const { formAnswers } = form;
+      return !!formAnswers.find((answer) => answer.userId === user.username);
+    }
+  };
+
+  const handleRedirect = () => {
+    navigate('/');
+  };
+
   return (
     <div>
       {form ? (
-        <div>
-          <Visualizer answerMode={true} completeForm={form} />
-        </div>
+        !hasAlreadyRespond ? (
+          <div>
+            <Visualizer answerMode={true} completeForm={form} />
+          </div>
+        ) : (
+          handleRedirect()
+        )
       ) : (
         <p>Ce formulaire n'existe pas</p>
       )}
