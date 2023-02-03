918c515251f18df7c593bc2278434e143a8741ba
Vincent Fontesse <vincent.fontesse@ac-dijon.fr>
2023-02-03 16:49:36 +0100

    wip: add pie chart in statistics page

--- app/imports/ui/pages/AnswersPage.jsx
+++ app/imports/ui/pages/AnswersPage.jsx
@@ -2,10 +2,14 @@ import { Button } from '@mui/material';
 import React from 'react';
 import { useState } from 'react';
 import { useLoaderData, useNavigate } from 'react-router-dom';
+import { Chart as ChartJS, ArcElement, Tooltip, Legend } from 'chart.js';
+import { Pie } from 'react-chartjs-2';
 
 // Cette page est a refactor !
 
 export const AnswersPage = () => {
+  ChartJS.register(ArcElement, Tooltip, Legend);
+
   const formFromBDD = useLoaderData();
   const finalArray = [];
   const statArray = [];
@@ -73,6 +77,41 @@ export const AnswersPage = () => {
     return cpt;
   };
 
+  function generateColor() {
+    const r = Math.random() * 200 + 50;
+    const g = Math.random() * 200 + 50;
+    const b = Math.random() * 200 + 50;
+
+    return `rgba(${r},${g},${b}, 1)`;
+  }
+
+  const generateChartData = (question) => {
+    const colors = [];
+    for (let i = 0; i < question.stat.length; i++) {
+      const color = generateColor();
+      colors.push(color);
+    }
+    const chartData = {
+      labels: question.stat.map((stat) => stat.answer),
+      datasets: [
+        {
+          label: 'Nombre de réponses: ',
+          data: question.stat.map((stat) => stat.count),
+          backgroundColor: colors.map((col) => col),
+          borderWidth: 1,
+        },
+      ],
+      options: {
+        plugins: {
+          legend: {
+            position: 'bottom',
+          },
+        },
+      },
+    };
+    return chartData;
+  };
+
   return (
     <>
       {hasNotAnswers() ? (
@@ -91,16 +130,27 @@ export const AnswersPage = () => {
                 (
                   question, // possible de ceer un composant stat
                 ) => (
-                  <div>
+                  <>
                     <h2>{question.questionTitle}</h2>
-                    {question.stat.map((stat) => (
-                      <ul>
-                        <li>
-                          <b>{stat.answer} </b>: {(stat.count / getAllCountStat(question.stat)) * 100}%
-                        </li>
-                      </ul>
-                    ))}
-                  </div>
+                    <div
+                      style={{
+                        display: 'flex',
+                        height: '35vh',
+                        width: '50vw',
+                        justifyContent: 'space-between',
+                        marginLeft: '4vw',
+                      }}
+                    >
+                      <div style={{ display: 'flex', flexDirection: 'column' }}>
+                        {question.stat.map((stat) => (
+                          <p>
+                            {stat.answer}: {(stat.count / getAllCountStat(question.stat)) * 100}%
+                          </p>
+                        ))}
+                      </div>
+                      <Pie data={generateChartData(question)} />
+                    </div>
+                  </>
                 ),
               )
             : finalArray.map(
