7c30a415a42e31dcfec6e1f9d47f53533e92f30c
Corentin Faisy <corentin.faisy@ac-dijon.fr>
2023-02-15 13:49:42 +0100

    fix

--- app/imports/ui/components/form/SubmitAnswerForm.jsx
+++ app/imports/ui/components/form/SubmitAnswerForm.jsx
@@ -1,6 +1,6 @@
 import { Button } from '@mui/material';
 import i18n from 'meteor/universe:i18n';
-import React, { useContext } from 'react';
+import React, { useContext, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { AnswerContext } from '../../contexts/AnswerContext';
 import { UserContext } from '../../contexts/UserContext';
