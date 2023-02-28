3cd959f038b3e7984085d6d767888d867ad526c8
Vincent Fontesse <vincent.fontesse@ac-dijon.fr>
2023-02-28 16:54:07 +0100

    feat(intro): add radio button for form type selection

--- app/imports/ui/components/FormInfoInputs.jsx
+++ app/imports/ui/components/FormInfoInputs.jsx
@@ -1,13 +1,20 @@
 import { i18n } from 'meteor/universe:i18n';
-import { Checkbox, FormControlLabel, FormGroup, TextField } from '@mui/material';
-import React from 'react';
+import { Checkbox, FormControlLabel, FormGroup, TextField, Radio, RadioGroup, FormControl } from '@mui/material';
+import React, { useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { addDesc, addTitle, toggleIsForGroup, toggleEditableAnswers, toggleIsPublic } from '../redux/slices/formSlice';
 
 export default function FormInfoInputs() {
   const form = useSelector((state) => state.form);
+  const [radioValue, setRadioValue] = useState('private');
   const dispatch = useDispatch();
   const isFormGroup = useSelector((state) => state.form.isForGroup);
+
+  const validateFormMode = (event) => {
+    setRadioValue(event.target.name);
+    if (radioValue === 'public') dispatch(toggleIsPublic());
+    if (radioValue === 'group') dispatch(toggleIsForGroup());
+  };
 
   return (
     <>
@@ -40,26 +47,29 @@ export default function FormInfoInputs() {
           label={i18n.__('component.formInfoInputs.editableAnswers')}
         />
       </FormGroup>
-      <FormGroup>
-        <FormControlLabel
-          disabled={form.isForGroup}
-          control={<Checkbox checked={form.isPublic} onChange={(e) => dispatch(toggleIsPublic())} name="isPublic" />}
-          label={i18n.__('component.formInfoInputs.formPublic')}
-        />
-      </FormGroup>
-      <FormGroup>
-        <FormControlLabel
-          disabled={form.isPublic}
-          control={
-            <Checkbox
-              checked={isFormGroup}
-              onChange={() => dispatch(toggleIsForGroup())}
-              name={i18n.__('component.formInfoInputs.formGroups')}
-            />
-          }
-          label={i18n.__('component.formInfoInputs.formGroups')}
-        />
-      </FormGroup>
+      <FormControl>
+        <RadioGroup
+          name="radio-buttons-group"
+          value={radioValue}
+          onChange={(e) => validateFormMode(e)}
+          defaultValue="private"
+        >
+          <FormControlLabel control={<Radio />} value="private" name="private" label="private" />
+
+          <FormControlLabel
+            control={<Radio />}
+            value="public"
+            name="public"
+            label={i18n.__('component.formInfoInputs.formPublic')}
+          />
+          <FormControlLabel
+            control={<Radio />}
+            value="group"
+            name="group"
+            label={i18n.__('component.formInfoInputs.formGroups')}
+          />
+        </RadioGroup>
+      </FormControl>
     </>
   );
 }
