9e51b930335b98db7ce5477248a557fc806b0874
Lionel MORIN <lionel.morin@ac-dijon.fr>
2023-09-14 17:37:58 +0200

    WIP: yo

--- app/imports/ui/components/form/InputBuilder.jsx
+++ app/imports/ui/components/form/InputBuilder.jsx
@@ -1,13 +1,16 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { Paper, Tooltip, Typography, Divider } from '@mui/material';
 import i18n from 'meteor/universe:i18n';
-import { useSelector } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
+import DragIndicatorIcon from '@mui/icons-material/DragIndicator';
 import { InputChoice } from './InputChoice';
 import ManageComponents from '../ManageComponents';
 import { LIST_OF_INPUT_BUILDER } from '../listOfInputBuilder';
+import { Reorder, useDragControls } from 'framer-motion';
 
 export const InputBuilder = () => {
   const form = useSelector((state) => state.form);
+  // const dispatch = useDispatch();
 
   const class1 = {
     display: 'flex',
@@ -23,6 +26,89 @@ export const InputBuilder = () => {
     maxHeight: '60vh',
   };
 
+  const [questions, setQuestions] = useState([]);
+
+  useEffect(() => {
+    if (form.components) {
+      setQuestions(form.components);
+    }
+  }, []);
+
+  const updateQuestionOrder = (lesQuestions) => {
+    // dispatch([...form.components]);
+    console.log('lesQuestions', lesQuestions);
+    setQuestions(lesQuestions);
+  };
+
+  function Question({ currentComponent, index }) {
+    const controls = useDragControls();
+
+    return (
+      <Reorder.Item value={index} dragListener={false} dragControls={controls}>
+        <Paper
+          sx={{
+            display: 'flex',
+            marginBottom: 1,
+            border: '1px black solid',
+            backgroundColor:
+              currentComponent.type === 'sectionStart'
+                ? 'LightSkyBlue'
+                : currentComponent.type === 'sectionEnd'
+                ? 'SpringGreen'
+                : currentComponent.type === 'separator'
+                ? 'orange'
+                : '',
+          }}
+        >
+          <div
+            style={{
+              display: 'flex',
+              width: '18vw',
+              paddingLeft: '0.5vw',
+              alignItems: 'center',
+              height: '5vh',
+            }}
+          >
+            <div className="reorder-handle" onPointerDown={(e) => controls.start(e)}>
+              <DragIndicatorIcon sx={{ color: 'rgb(180, 180, 180)', marginLeft: -1 }} />
+            </div>
+            {LIST_OF_INPUT_BUILDER.map(
+              (inputBuilder) =>
+                inputBuilder.id === currentComponent.type && (
+                  <div key={inputBuilder.id} style={{ display: 'flex', justifyContent: 'start' }}>
+                    <Tooltip title={i18n.__(`component.inputs.${inputBuilder.name}`)}>{inputBuilder.icon}</Tooltip>
+                    <Divider orientation="vertical" flexItem sx={{ margin: '0 1vw' }} />
+                  </div>
+                ),
+            )}
+            <p
+              style={{
+                maxHeight: '1.2rem',
+                whiteSpace: 'nowrap',
+                textOverflow: 'ellipsis',
+                overflowY: 'hidden',
+                overflow: 'hidden',
+              }}
+            >
+              {currentComponent.title}
+            </p>
+            {currentComponent.answerRequired && (
+              <Tooltip title={i18n.__('component.inputBuilder.inputRequired')}>
+                <Typography sx={{ marginLeft: 0.5 }} color="red" variant="h4">
+                  *
+                </Typography>
+              </Tooltip>
+            )}
+            <div></div>
+          </div>
+          <div>
+            <ManageComponents currentComponent={currentComponent} index={index} />
+          </div>
+        </Paper>
+      </Reorder.Item>
+    );
+  }
+
   return (
     <>
       <div style={{ display: 'flex', flexDirection: 'row', maxHeight: '100%', justifyContent: 'space-evenly' }}>
@@ -33,67 +119,11 @@ export const InputBuilder = () => {
         <div className={class1}>
           <h3>{i18n.__('component.inputBuilder.inputOrder')}</h3>
           <div className={class2}>
-            {form.components.map((currentComponent, index) => (
-              <Paper
-                sx={{
-                  display: 'flex',
-                  marginBottom: 1,
-                  border: '1px black solid',
-                  backgroundColor:
-                    currentComponent.type === 'sectionStart'
-                      ? 'LightSkyBlue'
-                      : currentComponent.type === 'sectionEnd'
-                      ? 'SpringGreen'
-                      : currentComponent.type === 'separator'
-                      ? 'orange'
-                      : '',
-                }}
-              >
-                <div
-                  style={{
-                    display: 'flex',
-                    width: '18vw',
-                    paddingLeft: '0.5vw',
-                    alignItems: 'center',
-                    height: '5vh',
-                  }}
-                >
-                  {LIST_OF_INPUT_BUILDER.map(
-                    (inputBuilder) =>
-                      inputBuilder.id === currentComponent.type && (
-                        <div key={inputBuilder.id} style={{ display: 'flex', justifyContent: 'start' }}>
-                          <Tooltip title={i18n.__(`component.inputs.${inputBuilder.name}`)}>
-                            {inputBuilder.icon}
-                          </Tooltip>
-                          <Divider orientation="vertical" flexItem sx={{ margin: '0 1vw' }} />
-                        </div>
-                      ),
-                  )}
-                  <p
-                    style={{
-                      maxHeight: '1.2rem',
-                      whiteSpace: 'nowrap',
-                      textOverflow: 'ellipsis',
-                      overflowY: 'hidden',
-                      overflow: 'hidden',
-                    }}
-                  >
-                    {currentComponent.title}
-                  </p>
-                  {currentComponent.answerRequired && (
-                    <Tooltip title={i18n.__('component.inputBuilder.inputRequired')}>
-                      <Typography sx={{ marginLeft: 0.5 }} color="red" variant="h4">
-                        *
-                      </Typography>
-                    </Tooltip>
-                  )}
-                  <div></div>
-                </div>
-                <div>
-                  <ManageComponents currentComponent={currentComponent} index={index} />
-                </div>
-              </Paper>
-            ))}
+            <Reorder.Group values={questions} as="div" onReorder={updateQuestionOrder}>
+              {questions.map((currentComponent, index) => (
+                <Question key={index} currentComponent={currentComponent} index={index} />
+              ))}
+            </Reorder.Group>
           </div>
         </div>
       </div>
