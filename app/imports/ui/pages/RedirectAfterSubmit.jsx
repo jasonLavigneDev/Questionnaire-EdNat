32388eba0f4717b9b406adb12648b597089de083
Vincent Fontesse <vincent.fontesse@ac-dijon.fr>
2024-01-04 11:08:58 +0100

    feat(redirect): add toast when copying url

--- app/imports/ui/pages/RedirectAfterSubmit.jsx
+++ app/imports/ui/pages/RedirectAfterSubmit.jsx
@@ -1,7 +1,7 @@
 import React, { useContext, useEffect, useState } from 'react';
 import { useLocation, useNavigate } from 'react-router-dom';
 import CheckCircleIcon from '@mui/icons-material/CheckCircle';
-import { Button } from '@mui/material';
+import { Alert, Button, Snackbar, Slide } from '@mui/material';
 import i18n from 'meteor/universe:i18n';
 import { UserContext } from '../contexts/UserContext';
 
@@ -17,6 +17,7 @@ const RedirectSubmitPage = () => {
   const location = useLocation();
   const { user } = useContext(UserContext);
   const [count, setCount] = useState(5);
+  const [openAlert, setOpenAlert] = useState(false);
 
   const urlForEdition = location.state?.urlForEdition || '';
   useEffect(() => {
@@ -32,8 +33,27 @@ const RedirectSubmitPage = () => {
     }
   }, [count]);
 
+  const handleCopyLinkClick = () => {
+    navigator.clipboard.writeText(urlForEdition);
+    setOpenAlert(true);
+  };
+
   return (
     <div style={flexCenterStyle}>
+      {openAlert && (
+        <Snackbar
+          open={openAlert}
+          autoHideDuration={5000}
+          onClose={() => setOpenAlert(false)}
+          anchorOrigin={{ vertical: 'top', horizontal: 'right' }}
+          TransitionComponent={Slide}
+          sx={{ marginTop: '5vh' }}
+        >
+          <Alert onClose={() => setOpenAlert(false)} severity="success" sx={{ width: '100%' }}>
+            {i18n.__('component.formActionButton.copyUrlSuccess')}
+          </Alert>
+        </Snackbar>
+      )}
       <h1 style={{ display: 'flex' }}>
         {i18n.__('page.redirectPage.title')}
         <CheckCircleIcon fontSize="large" sx={{ color: 'green', marginLeft: '1vw' }} />
@@ -42,7 +62,7 @@ const RedirectSubmitPage = () => {
       {urlForEdition && (
         <>
           <p>{i18n.__('page.redirectPage.copyUrl')}</p>
-          <Button variant="contained" onClick={() => navigator.clipboard.writeText(urlForEdition)}>
+          <Button variant="contained" onClick={() => handleCopyLinkClick()}>
             {i18n.__('page.redirectPage.copyButton')}
           </Button>
         </>
